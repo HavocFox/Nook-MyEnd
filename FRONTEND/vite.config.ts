128cfa36c699e1f3955b5a62fce5c628e60e8789
HavocFox <duskydraws@gmail.com>
2024-09-11 23:02:21 -0400

    first commit

--- FRONTEND/vite.config.ts
+++ FRONTEND/vite.config.ts
@@ -1,7 +1,11 @@
+// vite.config.js or vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Nook-MyEnd/', // Базовый путь для приложения
   plugins: [react()],
+  base: '/Nook-MyEnd/',  // Set base to match your deployment path
+  build: {
+    outDir: 'dist' // Ensure this matches the output directory expected by Vercel
+  }
 });
